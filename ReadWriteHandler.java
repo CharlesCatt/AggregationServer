02086865191e9625984bfcba82cbcc3baa02d642
CharlesCatt <Charles.Catt.98@gmail.com>
2019-09-18 22:08:35 +0930

    initial implementation of the ReadWrite handler threads

---
+++ ReadWriteHandler.java
@@ -0,0 +1,27 @@
+// For checking the queue of read/write requests and starting their execution
+import java.net.*;
+import java.io.*;
+import java.util.PriorityQueue;
+
+public class ReadWriteHandler implements Runnable {
+
+    private AggregationServer parentThread;
+    public ReadWriteHandler(AggregationServer parent){
+        parentThread = parent;
+    }
+    public void run() {
+        while(true) {
+
+            Thread t = parentThread.PQ.poll();
+            if (t != null) {
+                try {
+                    t.start();
+                    t.join();
+                } catch (InterruptedException e) {
+                    System.out.println(e);
+                }
+            }
+
+        }
+    }
+}
